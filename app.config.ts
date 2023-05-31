da543311f332a81ec934c578973900ca92d299d7
Jim Hawker <j.d.hawker@googlemail.com>
2023-05-31 14:32:13 +0100

    new logo

--- app.config.ts
+++ app.config.ts
@@ -12,7 +12,7 @@ export default defineAppConfig({
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        pathDark: '/affectiveAism.png', // path of the logo in dark mode, leave this empty if you want to use the same logo
         alt: 'alpine' // alt of the logo
       }
     },
